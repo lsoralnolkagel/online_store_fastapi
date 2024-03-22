eb66df9f4b59a2955d023268afc2d808e290ca35
SoraNoKage <llsoranokagell@gmail.com>
2024-03-22 15:52:29 +0300

    html template for registration

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,24 +1,43 @@
-import jwt
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, Request, HTTPException, Form
 from fastapi.security import HTTPBasic, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from fastapi.templating import Jinja2Templates
 from app.api.models.user import UserToRegister, UserToLogin
 from app.api.db.database import get_db
 from app.api.db.models import User
-from app.core.security import authenticate_user, create_access_token, get_current_user, get_password_hash
+from app.core.security import authenticate_user, create_access_token, get_user, get_password_hash
 
 
 user_router = APIRouter(prefix="/auth")
 security = HTTPBasic()
+templates = Jinja2Templates(directory="templates")
+
+
+@user_router.get("/register/")
+def show_register_form(request: Request):
+    return templates.TemplateResponse("registration.html", {"request": request})
 
 
 @user_router.post("/register/")
-def register_user(user: UserToRegister, db: Session = Depends(get_db)):
-    db_user = User(username=user.username, full_name=user.full_name, email=user.email, hashed_password=get_password_hash(user.password))
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return {"user created": db_user}
+def register_user(request: Request, username: str = Form(...), full_name: str = Form(), email: str = Form(...),
+                  password: str = Form(...), password_repeat: str = Form(...), db: Session = Depends(get_db)):
+    if request.method == "POST":
+        existing_user = get_user(username, db)
+        if existing_user:
+            return {"message": "user with the same username already exists"}
+        else:
+            used_email = db.query(User).filter(User.email == email).first()
+            if used_email:
+                return {"message": "this email is already used"}
+            else:
+                if password != password_repeat:
+                    return {"message": "passwords are different"}
+                else:
+                    db_user = User(username=username, full_name=full_name, email=email, hashed_password=get_password_hash(password))
+                    db.add(db_user)
+                    db.commit()
+                    db.refresh(db_user)
+                    return templates.TemplateResponse("suc_reg.html", {"request": request, "user_created": db_user})
 
 
 @user_router.post("/login/")
---
+++ main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from app.api.endpoints.users import user_router
+from app.api.endpoints.goods import goods_router
+from pages.router import router as router_pages
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def get_main():
+    return {"message": "Welcome to the best online store"}
+
+app.include_router(user_router)
+app.include_router(goods_router)
+app.include_router(router_pages)
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)

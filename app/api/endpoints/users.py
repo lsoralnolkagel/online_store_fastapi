4277cfee6b072e5ae4fd822c951b6e4e6cc143b0
SoraNoKage <llsoranokagell@gmail.com>
2024-03-25 22:45:31 +0300

    Templates for registration and login endpoints, without changes in security

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,24 +1,26 @@
-from fastapi import APIRouter, Depends, Request, HTTPException, Form
-from fastapi.security import HTTPBasic, OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, Request, HTTPException, Form, Header
+from fastapi.security import HTTPBasic, OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from typing import Optional
 from fastapi.templating import Jinja2Templates
 from app.api.models.user import UserToRegister, UserToLogin
 from app.api.db.database import get_db
 from app.api.db.models import User
-from app.core.security import authenticate_user, create_access_token, get_user, get_password_hash
+from app.core.security import authenticate_user, get_current_user, create_access_token, get_user, get_password_hash
 
 
 user_router = APIRouter(prefix="/auth")
 security = HTTPBasic()
 templates = Jinja2Templates(directory="templates")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@user_router.get("/register/")
+@user_router.get("/register")
 def show_register_form(request: Request):
     return templates.TemplateResponse("registration.html", {"request": request})
 
 
-@user_router.post("/register/")
+@user_router.post("/register")
 def register_user(request: Request, username: str = Form(...), full_name: str = Form(), email: str = Form(...),
                   password: str = Form(...), password_repeat: str = Form(...), db: Session = Depends(get_db)):
     if request.method == "POST":
@@ -40,11 +42,22 @@ def register_user(request: Request, username: str = Form(...), full_name: str = 
                     return templates.TemplateResponse("suc_reg.html", {"request": request, "user_created": db_user})
 
 
-@user_router.post("/login/")
-def login_user(user: UserToLogin):
-    username = user.username
-    password = user.password
-    if not authenticate_user(username, password):
-        raise HTTPException(status_code=401, detail="Invalid credentials", headers={"WWW-Authenticate": "Bearer"})
-    access_token = create_access_token(data={"sub": username})
-    return {"access_token": access_token, "token_type": "bearer"}
+@user_router.get("/login")
+def show_login_form(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@user_router.post("/login")
+def login_user(request: Request, username: str = Form(...), password: str = Form(...)):
+    if request.method == "POST":
+        if not authenticate_user(username, password):
+            return templates.TemplateResponse("error.html")
+        access_token = create_access_token(data={"sub": username})
+        return templates.TemplateResponse("suc_log.html", {"request": request, "access_token": access_token, "token_type": "bearer"})
+
+
+users_router = APIRouter(prefix="/users")
+
+
+@users_router.get("/me")
+def get_my_page(request: Request):
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from app.api.endpoints.users import user_router
+from app.api.endpoints.users import user_router, users_router
 from app.api.endpoints.goods import goods_router
 from pages.router import router as router_pages
 
@@ -16,6 +16,7 @@ async def get_main():
     return {"message": "Welcome to the best online store"}
 
 app.include_router(user_router)
+app.include_router(users_router)
 app.include_router(goods_router)
 app.include_router(router_pages)
 
